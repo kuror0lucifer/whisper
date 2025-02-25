172499a643aa6de39920910e0a3a63decbd2e71d
kuror0lucifer <kovalishin.2014@mail.ru>
2025-02-25 19:45:43 +0200

    optimizing

--- frontend/src/modules/games/components/GameList.tsx
+++ frontend/src/modules/games/components/GameList.tsx
@@ -1,29 +1,93 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { GameCard } from '../../../components/GameCard';
 import { baseImgURL } from '../../../constants/baseImgURL';
-import { useSelector } from 'react-redux';
-import { selectGames } from '../../../redux/games/selectors';
-import { useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { selectGames, selectQuery } from '../../../redux/games/selectors';
+import {
+  fetchGames,
+  setCurrentPage,
+  setStatus,
+} from '../../../redux/games/slice';
+import { AppDispatch, RootState } from '../../../redux/store';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { cleanTitle, cleanTitleForUrl } from '../utils/cleanTitle';
+import { Skeleton } from '../../../modules/games/components/Skeleton';
+import { Pagination } from '../../../components/Pagination';
 
 export const GameList: FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
   const games = useSelector(selectGames);
+  const { totalPages, status, currentPage } = useSelector(
+    (state: RootState) => state.games
+  );
+  const currentSearch = useSelector(selectQuery);
   const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const itemsPerPage = 20;
+
+  useEffect(() => {
+    const page = Number(searchParams.get('page')) || 1;
+    if (page !== currentPage + 1) {
+      dispatch(setCurrentPage(page - 1));
+    }
+  }, [searchParams, dispatch, currentPage, setSearchParams]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        await dispatch(
+          fetchGames({ page: currentPage, itemsPerPage })
+        ).unwrap();
+      } catch {
+        dispatch(setStatus('failed'));
+      }
+    };
+
+    loadData();
+  }, [dispatch, currentPage]);
+
+  const handlePageChange = ({ selected }: { selected: number }) => {
+    const newPage = selected + 1;
+    dispatch(setCurrentPage(selected));
+
+    if (newPage === 1) {
+      setSearchParams(
+        currentSearch ? { search: currentSearch, page: String(newPage) } : {}
+      );
+    } else {
+      setSearchParams({ search: currentSearch, page: String(newPage) });
+    }
+  };
 
   return (
-    <div className='w-full flex flex-wrap justify-center items-center gap-10 my-8 px-10'>
-      {games?.map(game => (
-        <GameCard
-          onClick={() =>
-            navigate(`/game/${game.nsuid}/${cleanTitleForUrl(game.title)}`)
-          }
-          key={game.objectID}
-          img={baseImgURL + game.productImage}
-          salePrice={game.price?.salePrice}
-          regPrice={game.price?.regPrice}
-          title={cleanTitle(game.title)}
-        />
-      ))}
-    </div>
+    <>
+      <div className='w-full flex flex-wrap justify-center items-center gap-10 my-8 px-10'>
+        {status === 'loading' ? (
+          Array.from({ length: 20 }).map((_, index) => <Skeleton key={index} />)
+        ) : (
+          <>
+            {games?.map(game => (
+              <GameCard
+                onClick={() =>
+                  navigate(
+                    `/game/${game.nsuid}/${cleanTitleForUrl(game.title)}`
+                  )
+                }
+                key={game.objectID}
+                img={baseImgURL + game.productImage}
+                salePrice={game.price?.salePrice}
+                regPrice={game.price?.regPrice}
+                title={cleanTitle(game.title)}
+              />
+            ))}
+          </>
+        )}
+      </div>
+      <Pagination
+        pageCount={totalPages}
+        onPageClick={handlePageChange}
+        forcePage={currentPage}
+      />
+    </>
   );
 };
--- frontend/src/pages/MainPage/components/MainPage.tsx
+++ frontend/src/pages/MainPage/components/MainPage.tsx
@@ -1,84 +1,15 @@
-import { FC, useEffect } from 'react';
+import { FC } from 'react';
 import { Header } from '../../../components/Header';
 import { GameList } from '../../../modules/games/components/GameList';
-import { useDispatch, useSelector } from 'react-redux';
-
-import {
-  fetchGames,
-  setCurrentPage,
-  setStatus,
-} from '../../../redux/games/slice';
-import { Skeleton } from '../../../modules/games/components/Skeleton';
-import { AppDispatch, RootState } from '../../../redux/store';
-import { Pagination } from '../../../components/Pagination';
 import { useAuth } from '../../../hooks/useAuth';
-import { useSearchParams } from 'react-router-dom';
-import { selectQuery } from '../../../redux/games/selectors';
 
 export const MainPage: FC = () => {
   const { isAuth } = useAuth();
-  const dispatch = useDispatch<AppDispatch>();
-  const { totalPages, status, currentPage } = useSelector(
-    (state: RootState) => state.games
-  );
-  const currentSearch = useSelector(selectQuery);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const itemsPerPage = 20;
-
-  useEffect(() => {
-    const page = Number(searchParams.get('page')) || 1;
-
-    if (page !== currentPage + 1) {
-      dispatch(setCurrentPage(page - 1));
-    }
-  }, [searchParams, dispatch, currentPage, setSearchParams]);
-
-  useEffect(() => {
-    const loadData = async () => {
-      try {
-        await dispatch(
-          fetchGames({ page: currentPage, itemsPerPage })
-        ).unwrap();
-      } catch {
-        dispatch(setStatus('failed'));
-      }
-    };
-
-    loadData();
-  }, [dispatch, currentPage]);
-
-  const handlePageChange = ({ selected }: { selected: number }) => {
-    const newPage = selected + 1;
-    dispatch(setCurrentPage(selected));
-
-    if (newPage === 1) {
-      setSearchParams(
-        currentSearch ? { search: currentSearch, page: String(newPage) } : {}
-      );
-    } else {
-      setSearchParams({ search: currentSearch, page: String(newPage) });
-    }
-  };
 
   return (
     <div className='w-full min-h-screen pb-5 bg-linear-to-b from-gray-100 to-blue-200'>
       <Header isAuth={isAuth} />
-      {status === 'loading' ? (
-        <div className='w-full flex flex-wrap justify-center items-center gap-10 my-8 px-10'>
-          {Array.from({ length: 20 }).map((_, index) => (
-            <Skeleton key={index} />
-          ))}
-        </div>
-      ) : (
-        <>
-          <GameList />
-          <Pagination
-            pageCount={totalPages}
-            onPageClick={handlePageChange}
-            forcePage={currentPage}
-          />
-        </>
-      )}
+      <GameList />
     </div>
   );
 };
